937b36fa3912ceadcb71601ae374fb38b2d50d18
Ceejay-777 <covenantcrackslord01@gmail.com>
2025-08-13 20:46:06 +0100

    landing

--- src/pages/landing/ContactUs.tsx
+++ src/pages/landing/ContactUs.tsx
@@ -2,15 +2,15 @@ import { LoginForm } from "@/components/login-form";
 
 const ContactUs = () => {
   return (
-    <div className="px-25 py-12">
-      <div className="flex items-center justify-around">
+    <div className="md:px-25 py-12 px-12">
+      <div className="flex items-center justify-around flex-col lg:flex-row gap-8">
         <h2
           className="bg-gradient-to-r from-gold-2  to-white-1
-           bg-clip-text text-transparent text-[100px] leading-[116px] font-medium font-playfair-display"
+           bg-clip-text text-transparent text-[100px] leading-[116px] font-medium font-playfair-display text-7xl md:text-[100px] md:leading-[116px] "
         >
           Contact us
         </h2>
-        <LoginForm className="w-[45%]" />
+        <LoginForm className="lg:w-[45%] w-full" />
       </div>
     </div>
   );
